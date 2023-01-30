30478f9599d71edeaf73d90b71ded27aa70bd7e7
Hassnain Ali <hassnainali1109@gmail.com>
2023-01-30 16:19:45 +0000

    edit chef page

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,33 +1,49 @@
 class ChefsController < ApplicationController
 
+def index
+  @chefs = Chef.all
+end
+
+
+
+
 def new
 	@chef = Chef.new
-	
 end
 
 def create
-    @chef= Chef.new(chef_params)
-    if @chef.save
-      flash[:success] = "Welcome #{@chef.chefname} to MyRecipes App!"
-      redirect_to chef_path(@chef) 
-
-    else
-      render 'new' , status: 308
-    end
+  @chef= Chef.new(chef_params)
+  if @chef.save
+   flash[:success] = "Welcome #{@chef.chefname} to MyRecipes App!"
+   redirect_to chef_path(@chef) 
+  else
+   render 'new' , status: 308
   end
+end
+
+def edit
+  @chef = Chef.find(params[:id])
+end
 
 
-  def show
-   @chef = Chef.find(params[:id])
-
+def update
+  @chef = Chef.find(params[:id])
+  if @chef.update(chef_params)
+   flash[:success] = "Chef account was updated successfully!"
+   redirect_to chef_path(@chef) 
+  else
+   render 'edit' , status: 308
   end
+end
+
+def show
+ @chef = Chef.find(params[:id])
+end
 
 
 private
 
-  def chef_params
-    params.require(:chef).permit(:chefname, :email, :password, :password_confirmation)
-  end
-
-
+ def chef_params
+  params.require(:chef).permit(:chefname, :email, :password, :password_confirmation)
+ end
 end
